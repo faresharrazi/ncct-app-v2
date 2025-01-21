3bf223b409f827605f134370796de0e910909661
Fares <m.fares.harrazi@gmail.com>
2025-01-21 22:24:15 +0200

    add filter sort and search

--- app/controllers/sub_account_transactions_controller.rb
+++ app/controllers/sub_account_transactions_controller.rb
@@ -7,13 +7,17 @@ class SubAccountTransactionsController < ApplicationController
     @transactions = @sub_account.sub_account_transactions
   end
 
- def all
+  def all
     @main_account = current_user.main_accounts.find_by(id: session[:selected_main_account_id])
     if @main_account.nil?
       redirect_to main_accounts_path, alert: "Please select a main account."
       return
     end
+
     @transactions = SubAccountTransaction.joins(:sub_account).where(sub_accounts: { main_account_id: @main_account.id }).includes(:sub_account)
+    @transactions = search_transactions(@transactions)
+    @transactions = filter_transactions(@transactions)
+    @transactions = sort_transactions(@transactions)
   end
 
   def show
@@ -113,6 +117,73 @@ class SubAccountTransactionsController < ApplicationController
 
   def set_transaction
     @sub_account_transaction = @sub_account.sub_account_transactions.find(params[:id])
+  end
+
+  def search_transactions(transactions)
+    if params[:search].present?
+      search_term = "%#{params[:search]}%"
+      transactions = transactions.where("LOWER(sub_account_transactions.title) LIKE ?", search_term.downcase)
+    end
+    transactions
+  end
+
+  def filter_transactions(transactions)
+    if params[:category_id].present?
+      transactions = transactions.where(category_id: params[:category_id])
+    end
+
+    if params[:sub_account_id].present?
+      transactions = transactions.where(sub_account_id: params[:sub_account_id])
+    end
+
+    if params[:creator_id].present?
+      transactions = transactions.where(creator_id: params[:creator_id])
+    end
+
+    if params[:start_date].present? && params[:end_date].present?
+      transactions = transactions.where(created_at: params[:start_date]..params[:end_date])
+    elsif params[:month].present?
+      begin
+        start_date = Date.strptime(params[:month], "%Y-%m").beginning_of_month
+        end_date = Date.strptime(params[:month], "%Y-%m").end_of_month
+        transactions = transactions.where(created_at: start_date..end_date)
+      rescue Date::Error
+        # Handle invalid date format
+        flash[:alert] = "Invalid date format"
+      end
+    end
+
+    transactions
+  end
+
+  def sort_transactions(transactions)
+    case params[:sort]
+    when "amount_asc"
+      transactions = transactions.order(amount: :asc)
+    when "amount_desc"
+      transactions = transactions.order(amount: :desc)
+    when "date_asc"
+      transactions = transactions.order(created_at: :asc)
+    when "date_desc"
+      transactions = transactions.order(created_at: :desc)
+    when "title_asc"
+      transactions = transactions.order(:title)
+    when "title_desc"
+      transactions = transactions.order(title: :desc)
+    when "creator_asc"
+      transactions = transactions.joins(:creator).order(Arel.sql("users.first_name || ' ' || users.last_name ASC"))
+    when "creator_desc"
+      transactions = transactions.joins(:creator).order(Arel.sql("users.first_name || ' ' || users.last_name DESC"))
+    when "account_asc"
+      transactions = transactions.joins(:sub_account).order(Arel.sql("sub_accounts.title ASC"))
+    when "account_desc"
+      transactions = transactions.joins(:sub_account).order(Arel.sql("sub_accounts.title DESC"))
+    when "category_asc"
+      transactions = transactions.joins(:category).order(Arel.sql("categories.title ASC"))
+    when "category_desc"
+      transactions = transactions.joins(:category).order(Arel.sql("categories.title DESC"))
+    end
+    transactions
   end
 
   def sub_account_transaction_params
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def owner_or_partner?(main_account)
     main_account.owners.include?(current_user)
   end
+
+  def toggle_sort(current_sort, column)
+    if current_sort == "#{column}_asc"
+      "#{column}_desc"
+    else
+      "#{column}_asc"
+    end
+  end
 end
